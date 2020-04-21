1672f6f762785ceecdd0f9ec8ea61f458c78ef39
mcn21 <60136227+mcn21@users.noreply.github.com>
2020-04-22 00:12:07 +0700

    Add files via upload

---
+++ tooheck.py
@@ -0,0 +1,228 @@
+import os
+import sys
+import time
+
+os.system('clear')
+time.sleep (1)
+os.system  ( 'figlet  TooHack')
+print "    \033[91m##$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$##"
+print "   \033[91m && \033[94mAuthor   : mcn21                     \033[91m &&"
+print " \033[91m   && \033[94mGithub   : https://github.com/mcn21 \033[91m  && "
+print "    \033[91m##$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$##"
+print
+time.sleep(1)
+print "\033[94m'[+] Menu Pilihan [+]"
+time.sleep(1)
+print "\033[92m[01] dark- vpro"
+time.sleep(1)
+print "[02] VIRTEX"
+time.sleep(1)
+print "[03] hack-facebook-teman"
+time.sleep(1)
+print "[04] Mr.Rv1.1"
+time.sleep(1)
+print "[05] Hack IG" 
+time.sleep(1)
+print "[06] pulsa free work100%" 
+time.sleep(1)
+print "[07] TamfanFacebook"
+time.sleep(1)
+print "[08] Spam Call" 
+time.sleep(1)
+print "[09] Trojans"
+time.sleep(1)
+print "[10] FBNEWV2"
+time.sleep(1)
+print "[11] LITESPAM"
+time.sleep(1)
+print "[12] Unix "
+time.sleep(1)
+print "[13] Tools check bin"
+time.sleep(1)
+print "[14] Tools spam bom"
+time.sleep(1)
+print "[15] kouta free for tree"
+time.sleep(1)
+print "[16] Bahan-bahan" 
+time.sleep(1)
+pilih = raw_input ( ' [?] Pilih: ' )
+if pilih == "01" :
+           os.system ('git clone https://github.com/m4rche3ll-cyber/dark-vpro') 
+           time.sleep(1)
+           print "\033[92m[~] Install Selesai" 
+           time.sleep(1)
+           print "\033[33m[~] selanjutnya ketik/copy $ cd dark-vpro" 
+           time.sleep(1)
+           print "[~] lalu ketik/copy $ chmod 777 dark-vpro.py" 
+           time.sleep(1)
+           print "[~] lalu ketik/copy $ python2 dark-vpro.py"
+           time.sleep(1)
+elif pilih =="02" :    
+           os.system('git clone https://github.com/muhammadfathul/VIRTEX') 
+           time.sleep(1)
+           print "\033[92m[~] Install Selesai" 
+           time.sleep(1)
+           print "\033[33m[~] selanjutnya ketik/copy $ cd VIRTEX" 
+           time.sleep(1)
+           print "[~] lalu ketik/copy $ chmod +x virtex.sh" 
+           time.sleep(1)
+           print "[~] lalu ketik/copy $ sh virtex.sh " 
+           time.sleep(1)
+elif pilih =="03" :      
+           os.system('git clone https://github.com/blackcodercrush/hack-facebook-teman') 
+           time.sleep(1)
+           print "\033[92m[~] Install Selesai" 
+           time.sleep(1)
+           print "\033[33m[~] selanjutnya ketik/copy $ cd hack-Facebook-teman" 
+           time.sleep(1)
+           print "[~] lalu ketik/copy $ sh requests.sh" 
+           time.sleep(1)
+           print "[~] lalu ketik/copy $ python2 hack-fb.py" 
+           time.sleep(1)
+elif pilih =="04" :    
+           os.system('git clone https://github.com/Mr-R225/Mr.Rv1.1') 
+           time.sleep(1)
+           print "\033[92m[~] Install Selesai" 
+           time.sleep(1)
+           print "\033[33m[~] selanjutnya ketik/copy $ cd Mr.Rv1.1" 
+           time.sleep(1)
+           print "[~] lalu ketik/copy $ sh Mr.Rv1.1.sh" 
+           time.sleep(1)
+elif pilih == "05" :
+           os.system('git clone https://github.com/darkcurut08/darkcurut08' ) 
+           time.sleep(1)
+           print "\033[92m[~] Install Selesai" 
+           time.sleep(1)
+           print "\033[33m[~] selanjutnya ketik/copy $ cd darkcurut08"
+           time.sleep(1)
+           print "[~] lalu ketik/copy $ sh Moreno77.sh" 
+           time.sleep(1)
+           print "[~] lalu ketik/coly $ python2 hack-fb.py" 
+           time.sleep(1)
+elif pilih =="06" :
+           os.system('git clone https://github.com/InYourG00D1/PulsaFree') 
+           time.sleep(1)
+           print "\033[92m[~] Install Selesai" 
+           time.sleep(1)
+           print "\033[33m[~] selanjutnya ketik/copy $ cd PulsaFree" 
+           time.sleep(1)
+           print "[~] lalu ketik/copy $ sh Free.sh"
+           time.sleep(1)
+elif pilih =="07"  :
+           os.system('git clone https://github.com/MrTamfanX/TamfanFacebook') 
+           time.sleep(1)
+           print "\033[92m[~] Install Selesai" 
+           time.sleep(1)
+           print "[\033[33m~] selanjutnya ketik/copy $ cd TamfanFacebook" 
+           time.sleep(1)
+           print "[~] lalu ketik/copy $ chmod +x *" 
+           time.sleep(1)
+           print "[~] lalu ketik/coly $ chmod -R 775 lib"
+           time.sleep(1)
+           print "[~] lalu ketik/coly $ sh install.sh" 
+           time.sleep(1)
+           print "[~] lalu ketik/coly $ sh tamfan.sh" 
+           time.sleep(1)
+elif pilih == "08" :
+           os.system('git clone https://github.com/InYourG00D1/InstallPlerN2') 
+           time.sleep(1)
+           print "\033[92m[~] Install Selesai" 
+           time.sleep(1)
+           print "\033[33m[~] selanjutnya ketik/copy $ cd SpamSms" 
+           time.sleep(1)
+           print "[~] lalu ketik/copy $ php SpamCall.php" 
+           time.sleep(1)
+elif pilih == "09" :
+           os.system('git clone https://github.com/R133F/Trojans') 
+           time.sleep(1)
+           print "\033[92m[~] Install Selesai" 
+           time.sleep(1)
+           print "\033[33m[~] selanjutnya ketik/copy $ cd Trojans" 
+           time.sleep(1)
+           print "[~] lalu ketik/copy $ python2 trojans.py"
+           time.sleep(1)
+elif pilih =="10" :
+           os.system('git clone https://github.com/MRA27/FBNEWV2') 
+           time.sleep(1)
+           print "\033[92m[~] Install Selesai" 
+           time.sleep(1)
+           print "\033[33m[~] selanjutnya ketik/copy $ cd FBNEWV2" 
+           time.sleep(1)
+           print "[~] lalu ketik/copy $ python2 MRA27.py" 
+           time.sleep(1)
+elif pilih == "11'":
+           os.system('git clone https://github.com/4L13199/LITESPAM') 
+           time.sleep(1)
+           print "\033[92m[~] Install Selesai" 
+           time.sleep(1)
+           print "\033[33m[~] selanjutnya ketik/copy $ cd LITESPAM" 
+           time.sleep(1)
+           print "[~] lalu ketik/copy $ sh LITESPAM.sh" 
+           time.sleep(1)
+elif pilih == "12" :
+           os.system('git clone https://github.com/InYourG00D1/Unix') 
+           time.sleep(1)
+           print "\033[92m[~] Install Selesai" 
+           time.sleep(1)
+           print "\033[33m[~] selanjutnya ketik/copy $ cd Unix" 
+           time.sleep(1)
+           print "[~] lalu ketik/copy $ sh KD-FB.sh" 
+           time.sleep(1)
+elif pilih == "13" :
+           os.system('git clone https://github.com/InYourG00D1/check') 
+           time.sleep(1)
+           print "\033[92m[~] Install Selesai" 
+           time.sleep(1)
+           print "\033[33m[~] selanjutnya ketik/copy $ cd check" 
+           time.sleep(1)
+           print "[~] lalu ketik/copy $ php bin.php" 
+           time.sleep(1)
+elif pilih == "14" :
+           os.system('git clone https://github.com/InYourG00D1/Spam') 
+           time.sleep(1)
+           print "\033[92m[~] Install Selesai" 
+           time.sleep(1)
+           print "\033[33m[~] selanjutnya ketik/copy $ cd Spam" 
+           time.sleep(1)
+           print "[~] lalu ketik/copy $ sh ms.sh" 
+           time.sleep(1)
+elif pilih == "15" :
+           os.system('git clone https://github.com/InYourG00D1/KuotaFree') 
+           time.sleep(1)
+           print "\033[92m[~] Install Selesai" 
+           time.sleep(1)
+           print "\033[33m[~] selanjutnya ketik/copy $ ccd KuotaFree" 
+           time.sleep(1)
+           print "[~] lalu ketik/copy $ sh Free.sh" 
+           time.sleep(1)
+elif pilih == "16" :
+	       os.system('clear')
+	       print  " \033[95mLODING" 
+	       time.sleep(1)
+	       print "\033[94mSabar asu gw lupa bahannya..!!! " 
+	       time.sleep(2)
+	       print " \033[92moke... Mulai install" 
+	       time.sleep(3)
+	       os.system ('clear') 
+	       os.system ('pkg update && pkg upgrade') 
+	       os.system ('pkg install git') 
+	       os.system ('pkg install python') 
+	       os.system ('pkg install python2') 
+	       os.system ('pkg install php') 
+	       os.system ('pkg install curl') 
+	       os.system ('pkg install wget') 
+	       os.system ('pkg install figlet') 
+	       os.system ('pkg install lolcat') 
+	       os.system ('pkg install ruby') 
+	       os.system ('pip2 install --upgrade pip') 
+	       os.system ('pip2 install requests') 
+	       os.system ('pip2 install mechanize') 
+	       os.system ('pkg install nano') 
+	       os.system ('pkg install cowsay') 
+	       os.system ('pkg install toilet') 
+	       os.system ('pkg install gem') 
+	       os.system ('pkg install bash') 
+	       os.system ('pkg install sh') 
+	       os.system ('gem install lolcat') 
+	       os.system ('pip2 install termcolor') 
+	       
